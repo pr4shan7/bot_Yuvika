f41406e0fa45e95e6017438429321ef62b84e970
Prashant Tripathi <pr4shan7@gmail.com>
2019-07-09 21:57:06 +0530

    added set ds; performance improvements

--- init.py
+++ init.py
@@ -8,3 +8,33 @@ api = tweepy.API(auth)
 
 # me = api.me()
 p7 = 'pr4shan7'
+p7_id = api.get_user(p7).id
+# print(type(p7_id))
+
+
+def set_toFollow():
+    s = set()
+    for follower in tweepy.Cursor(api.followers).items():
+        s.add(follower.id)
+    for follower in tweepy.Cursor(api.followers, p7).items():
+        s.add(follower.id)
+    return s
+
+
+follow_list = set_toFollow()
+# print(len(follow_list))
+
+
+def set_following():
+    s = set()
+    for friend in tweepy.Cursor(api.friends).items():
+        s.add(friend.id)
+    return s
+
+
+following_list = set_following()
+# print(len(following_list))
+
+
+""" rate_limit = api.rate_limit_status()
+print(rate_limit) """
